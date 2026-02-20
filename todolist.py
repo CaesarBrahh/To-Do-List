1e904fb8c7b00dc3799e15689398e472e71ff6d7
Julian Ware <julaenw@gmail.com>
2026-02-20 17:23:04 -0600

    python scaffold

--- todolist.py
+++ todolist.py
@@ -1,4 +1,5 @@
 def main:
+
 	# print to-do list
 
 	# ask for user action
@@ -6,8 +7,33 @@ def main:
 	# perform action
 
 	# re-loop
-	
+
+	todolist = []
+
+	while True:
+		# display to do list
+		display(todolist)
+
+		action = input("Type A to add, R to remove, Q to quit: ").lower()
+
+		if action == "a":
+			# add
+		elif action == "r":
+			# remove
+		elif action == "q":
+			# quit
+		else:
+			print("Improper usage.")
+			continue
+
 	return 0
+
+def display():
+	print("To-Do List:")
+	i = 1
+	for item in todolist:
+		print(f"{i}.) {item}")
+		i++
 
 if __name__ == "__main__":
 	main()
