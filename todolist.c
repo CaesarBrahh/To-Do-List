dfb22d77ad9c6b4a91dc1fe2c0f67fc95dabd221
Julian Ware <julaenw@gmail.com>
2026-02-20 18:42:25 -0600

    C print to-do list

--- todolist.c
+++ todolist.c
@@ -4,19 +4,34 @@
 
 #define MAX_ITEMS_AMOUNT 99
 
+int display(todolist, todolist_length);
+
 int main(void)
 {
 	// define and instantiate list
-	char *list_items[MAX_ITEMS_AMOUNT];
+	char *todolist[MAX_ITEMS_AMOUNT];
+	todolist_length = 0;
 
 	// Start loop
 	while (1)
 	{
 		// print to-do list
+		display(todolist, todolist_length);
 
 		// ask for user action
 
 		// perform action
 	}
 	// re-loop
+}
+
+int display(todolist, todolist_length)
+{
+	printf("To-Do List:");
+	for (int i = 0; i < todolist_length; i++)
+	{
+		printf("%i.) %s\n", i+1, todolist[i]);
+	}
+
+	return 0;
 }
